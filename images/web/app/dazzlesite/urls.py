3f230dad5ac9fd3206fee2c9d4c4f04d4da03524
Grayson Miller <grayson.miller124@gmail.com>
2023-06-23 22:25:25 -0600

    static stuff

--- images/web/app/dazzlesite/urls.py
+++ images/web/app/dazzlesite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from dazzlesite.views import dizzy
 from dict_builder.views import dict_builder
@@ -24,4 +26,4 @@ urlpatterns = [
     # simple call to dizzy-request.py
     path("dizzy/", dizzy.request, name="dizzy"),
     path("dict-builder/", dict_builder, name="dict_builder"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
